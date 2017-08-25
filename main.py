ddf853f984d6c145a8a2a95153b456b0e321ba3e
ysicing <root@ysicing.net>
2017-08-25 11:32:59 +0800

    init

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
-# coding=utf-8
-# Version:0.0.1
-# Author: ysicing
-# file: main.py
-# time: 2017/7/18 23:09
-# Copyright 2017 ysicing. All Rights Reserved.
-# 
+
+# Copyright 2015 Conchylicultor. All Rights Reserved.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -17,34 +13,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ------------------------------------------------------------------------
+# ==============================================================================
 
-import argparse
+"""
+Main script. See README.md for more information
 
-def parse_args():
-    parse = argparse.ArgumentParser(description='Intelligent robot based on TensorFlow.')
+Use python 3
+"""
 
-    help_ = 'you can set this value in terminal,but now only support chat.'
-    parse.add_argument('-t','--target',default='chat',choices=['chat'],help=help_)
+from chatbot import chatbot
 
-    help_ = 'choose to train or test.'
-    parse.add_argument('--train',dest='train',action='store_true',help=help_)
-    parse.add_argument('--no-train',dest='train',action='store_false',help=help_)
-    parse.set_defaults(train=True)
 
-    args_ = parse.parse_args()
-    return args_
-
-if __name__ == '__main__':
-    args =parse_args()
-    if args.target == 'chat':
-        from inference import ergo
-        if args.train:
-            ergo.main(True)
-        else:
-            ergo.main(False)
-    elif args.target == 'poem':
-        print('[INFO] The next update may be supported')
-        pass
-    else:
-        print('[INFO] Simple english communion right now.')
+if __name__ == "__main__":
+    chatbot = chatbot.Chatbot()
+    chatbot.main()
